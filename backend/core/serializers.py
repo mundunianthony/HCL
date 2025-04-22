62fb23f53a06f99f71565f1568a7eedaef365a1a
munduni anthony <mundunianthony@gmail.com>
2025-04-22 12:58:10 +0300

    update serializers

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -1,12 +1,14 @@
 # core/serializers.py
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model  # Import get_user_model
 from rest_framework import serializers
+
+User = get_user_model()  # Use the custom user model
 
 class UserRegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = User  # Use the custom user model
         fields = ('username', 'email', 'password')
 
     def create(self, validated_data):
