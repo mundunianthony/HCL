f6dcbb7bb061b7c62b38762592231f4ad053380c
munduni anthony <mundunianthony@gmail.com>
2025-04-22 11:27:24 +0300

    register models in admin.py

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register models
+from django.contrib import admin
+from .models import User, HealthCenter, Emergency
+
+admin.site.register(User)
+admin.site.register(HealthCenter)
+admin.site.register(Emergency)
