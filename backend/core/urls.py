807333c1a162e0221c40dd2424ff4123543d3f8c
munduni anthony <mundunianthony@gmail.com>
2025-04-22 12:34:38 +0300

    update urls.py

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,7 +1,11 @@
 # core/urls.py
 from django.urls import path
-from .views import nearby_hospitals
+from .views import nearby_hospitals, RegisterView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('nearby-hospitals/', nearby_hospitals, name='nearby-hospitals'),
 ]
