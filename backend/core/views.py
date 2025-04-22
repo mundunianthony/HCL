e4208fd06b1ad4d0e5276dfea675a558ab52d457
munduni anthony <mundunianthony@gmail.com>
2025-04-22 12:37:03 +0300

    refactor: update user model reference in RegisterView to use get_user_model

--- backend/core/views.py
+++ backend/core/views.py
@@ -6,7 +6,7 @@ from django.http import JsonResponse
 from .utils.location import get_user_location, get_nearby_hospitals
 from rest_framework import generics
 from .serializers import UserRegisterSerializer
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model  # Import get_user_model
 from rest_framework.permissions import AllowAny
 
 def nearby_hospitals(request):
@@ -18,6 +18,6 @@ def nearby_hospitals(request):
         return JsonResponse({'error': 'Unable to determine location'}, status=400)
 
 class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
+    queryset = get_user_model().objects.all()  # Use get_user_model to reference the custom user model
     serializer_class = UserRegisterSerializer
     permission_classes = [AllowAny]
