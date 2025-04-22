2bee4303ab885107113978cc7bee0c632e342703
munduni anthony <mundunianthony@gmail.com>
2025-04-22 12:28:30 +0300

    add UserRegisterSerializer for user registration

---
+++ backend/core/serializers.py
@@ -0,0 +1,14 @@
+# core/serializers.py
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
