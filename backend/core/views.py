2b2ea89310a4604c26e535d47d8c320afb7a9b73
munduni anthony <mundunianthony@gmail.com>
2025-04-22 11:44:31 +0300

    implement nearby hospitals view to return user location and nearby hospitals

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+# core/views.py
+from django.http import JsonResponse
+from .utils.location import get_user_location, get_nearby_hospitals
+
+def nearby_hospitals(request):
+    lat, lon = get_user_location()
+    if lat and lon:
+        hospitals = get_nearby_hospitals(lat, lon)
+        return JsonResponse({'location': {'lat': lat, 'lon': lon}, 'hospitals': hospitals})
+    else:
+        return JsonResponse({'error': 'Unable to determine location'}, status=400)
